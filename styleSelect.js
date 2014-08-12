1f9042a7ff715368b95a082024b9c96ccb493205
Robert Brako <brako008@umn.edu>
2014-08-12 12:14:57 -0500

    Major Update 1: more images, new text, and css added.

---
+++ styleSelect.js
@@ -0,0 +1,19 @@
+/** 
+script to select appropriate stylesheet
+
+ISSUE: should this be addressed with PHP?  Probably...
+
+-ok, this is off-base, but MPR is doing a piece on pseudofolliculitis barbae,
+and I MUST take notes on it.  Hopefully my face can be smooth once again...
+
+notes:
+Shaving product: Bevel by Walker & Co. shaving system - good for anyone, it seems
+(nair... depillatory cream => don't use!)
+
+**/
+
+function changeStyle (newStyle) {
+//  if desktop (nice one like mine) { }
+//  else if mobile { }
+//  else default () { }
+}

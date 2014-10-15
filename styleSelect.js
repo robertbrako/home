c42c0786866e21570281d0ec7888167119772a54
Robert Brako <robertbrako@users.noreply.github.com>
2014-10-15 13:07:15 -0500

    Update styleSelect.js
    Prelim work on testing for window size before applying style sheet.

--- styleSelect.js
+++ styleSelect.js
@@ -12,6 +12,36 @@ Shaving product: Bevel by Walker & Co. shaving system - good for anyone, it seem
 
 **/
 
+/** Add the following to index.html:
+ * <link rel="stylesheet" type="text/css" href="narrowStyle.css" title="narrowStyle"/>
+ * <link rel="stylesheet" type="text/css" href="medStyle.css" title="medStyle"/>
+ * <link rel="stylesheet" type="text/css" href="wideStyle.css" title="wideStyle"/>
+ * consider making a separete css folder
+**/
+/** Get browser width robust function:
+ *function getBrowserWidth() {
+    if (window.innerWidth) { return window.innerWidth;}
+    else if (document.documentElement && document.documentElement.clientWidth != 0) {
+      return document.documentElement.clientWidth; }
+    else if (document.body) { return document.body.clientWidth;}
+    return 0;
+  }
+**/
+function monitorResizeTest() { // platform-dependent
+  // if window.width is between [0,400], [400,800], [800,9999] { use different styleSheet; }
+  if (window.outerWidth <= 480) {
+    if (window.outerWidth >= 0)
+      alert("narrow");
+  }
+  else {
+    if (window.outerWidth <= 800)
+      alert("medium");
+    else
+      alert("large");
+  }
+  // is window.outerWidth ever -1, or less than 0 generally?
+}
+
 function changeStyle (newStyle) {
 //  if desktop (nice one like mine) { }
 //  else if mobile { }
